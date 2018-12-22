0a41c2f0a0ddb025a6ee03716a9a41bd4b22bf27
Aymerick Valette <valaymerick@gmail.com>
2018-12-22 19:09:04 +0100

    Use a global stylesheet

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure } from '@storybook/react'
 
+import './global.css'
+
 const req = require.context('../src', true, /stories\.tsx$/)
 const loadStories = () => req.keys().forEach(req)
 
