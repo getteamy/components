4a80a25c35c00669674a14087934ef3fe9f054c5
Aymerick Valette <valaymerick@gmail.com>
2018-12-29 13:26:08 +0100

    Add button stories

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from './'
-
-export enum variations {
-  PRIMARY = 'primary',
-  DANGER = 'danger',
-  SUCCESS = 'success',
-  DISCREET = 'discreet'
-}
+import { VARIATIONS } from './style';
 
 storiesOf('Button', module)
   .add('primary with text', () => (
-    <Button>Submit</Button>
+    <Button variation={VARIATIONS.PRIMARY}>Submit</Button>
+  ))
+  .add('danger with text', () => (
+    <Button variation={VARIATIONS.DANGER}>Submit</Button>
+  ))
+  .add('link with text', () => (
+    <Button variation={VARIATIONS.LINK}>Submit</Button>
+  ))
+  .add('secondary with text', () => (
+    <Button variation={VARIATIONS.SECONDARY}>Submit</Button>
   ))
